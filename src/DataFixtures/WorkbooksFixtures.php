59dfd855b237c3fda29e38cbfe98ce5725dd4606
olisimu <simuenstabretagne@gmail.com>
2020-04-23 18:04:24 +0200

    Mise à jour des dépendances dans fixture

--- src/DataFixtures/WorkbooksFixtures.php
+++ src/DataFixtures/WorkbooksFixtures.php
@@ -4,14 +4,14 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Entity\Workbook;
+use App\Entity\Workbook;
+use Faker\Factory;
 
 class WorkbooksFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $faker = Faker\Factory::create('FR_fr');
-        
+        $faker = Factory::create('FR_fr');
         
         for($i = 0;$i<3;$i++) {
             $wkb = new Workbook();
